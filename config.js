cf342ada16682c2ed6e8b668c3e83ed4fc2571ef
411danger <jessemania411@gmail.com>
2024-11-12 07:30:03 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const { color } = require('./lib/myfunc')
 
 //owner
 global.owner = '2347043759577'
-global.nomerowner = ["2347043759577"]
+global.nomerowner = ["2349121530876"]
 
 // Apikey 
 
@@ -27,7 +27,7 @@ global.apilinode = ''// apikey vps account linode
 global.apitokendo = ''
 
 //database 
-global.urldb = ''; // just leave it blank but if you want to use the mongo database, fill in the mongo url
+global.urldb = '{"noiseKey":{"private":{"type":"Buffer","data":"yNTgxZE0pPipTgFkU/ZC/HR8knQBc4dHAetQvupQLWc="},"public":{"type":"Buffer","data":"ftiJe4/ghcQN1/JeCKWL8Uq8igKMY2A+mcQ9qNhy9go="}},"pairingEphemeralKeyPair":{"private":{"type":"Buffer","data":"eA7r9uVy6in/S3V+DxWlIBvce+o6hNgxM15Ye6MIM0M="},"public":{"type":"Buffer","data":"IH9+o1augRlshfEgY5WpSTrKVK02EDqvIeTLZ9DYomA="}},"signedIdentityKey":{"private":{"type":"Buffer","data":"kFPVIFo6Ha8Kr+Z8q4zTodQCfgXTI1eoAovHsPpTtHA="},"public":{"type":"Buffer","data":"VVokMHWOed0Du+ITbwFXky3Uqt62NnaBekZiTb3WOD4="}},"signedPreKey":{"keyPair":{"private":{"type":"Buffer","data":"kABxVUWDmnwupmqQWkfIc1fC+93O/WtTpsymdWsqflI="},"public":{"type":"Buffer","data":"tnw6bRd53t4nCrCN6Pue3tHL+8O6Vh3HOT3qamXOJjU="}},"signature":{"type":"Buffer","data":"i4UMOq7QruJ64AnywNbWo02MDVsJr8P5UprkujNW4SYTxr83hYk2wN7WC2S4f3r//n3IGM4BX2kwWUwVLbbdgw=="},"keyId":1},"registrationId":165,"advSecretKey":"xwxVNraaPDWIQRvOHqvYIvfWZJFZR3xzfAMMJ6d0CKI=","processedHistoryMessages":[{"key":{"remoteJid":"2349121530876@s.whatsapp.net","fromMe":true,"id":"47DBF5C5DA8BF4B449CF1B465CAAF9A8"},"messageTimestamp":1731391785}],"nextPreKeyId":31,"firstUnuploadedPreKeyId":31,"accountSyncCounter":0,"accountSettings":{"unarchiveChats":false},"deviceId":"Y_kd5PoBRuiGIgFF742MEg","phoneId":"a59d39fc-fc3f-4881-88f4-1283829bdb85","identityId":{"type":"Buffer","data":"fP14uWlggYHmCwKoEQ8aUW7s+g0="},"registered":true,"backupToken":{"type":"Buffer","data":"ZD5/GiSXVKPXYWrbKOyfs1WQ1KY="},"registration":{},"pairingCode":"E44WPBXH","me":{"id":"2349121530876:10@s.whatsapp.net","lid":"118962490748980:10@lid","name":"Jesse Emmanuel 2"},"account":{"details":"CIXO1AkQnuLLuQYYAyAAKAA=","accountSignatureKey":"6TngoHvoIF4GARz3ybWFWJqcLHxirZaOgVohhM1mDEU=","accountSignature":"DNjXeMZeSqfxx4631SPOexoqdBOxY17BetsVndn8yCXzJjNHTLe+USgCQR74F6yIBQT94BracEtknjK5Bex3BA==","deviceSignature":"69hf8vUH+zZXyn5ovGOte6+21ojVIKdvcQvIz5mvFyw2+6G8gUVhR6wcBr4bl1GZyCvt/4qhutI+aV7yPhCPjQ=="},"signalIdentities":[{"identifier":{"name":"2349121530876:10@s.whatsapp.net","deviceId":0},"identifierKey":{"type":"Buffer","data":"Bek54KB76CBeBgEc98m1hVianCx8Yq2WjoFaIYTNZgxF"}}],"platform":"android","lastAccountSyncTimestamp":1731391777,"myAppStateKeyId":"AAAAAPON"}'; // just leave it blank but if you want to use the mongo database, fill in the mongo url
 
 global.limitawal = {
     premium: "Infinity",
